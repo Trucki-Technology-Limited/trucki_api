7f71a1618f549e8c5ec6728499c4ae720c88b93b
kunle.edun <kunle.edun@sterling.ng>
2024-03-05 14:29:47 +0100

    updates

---
+++ trucki/Entities/Company.cs
@@ -0,0 +1,14 @@
+﻿namespace trucki.Entities
+{
+    public class Company : BaseClass
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+        // Navigation properties
+        public ICollection<Driver> Drivers { get; set; } = new List<Driver>();
+        public ICollection<Manager> Managers { get; set; } = new List<Manager>();
+    }
+}
