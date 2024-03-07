d2e5bd5d88bafca700fb2dfd23bee997a2a4d780
kunle.edun <kunle.edun@sterling.ng>
2024-03-07 09:51:20 +0100

    features update on Truck and Truck model  added

---
+++ trucki/Entities/TruckOwner.cs
@@ -0,0 +1,11 @@
+﻿namespace trucki.Entities
+{
+    public class TruckOwner : BaseClass
+    {
+        public string CompanyName { get; set; } 
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string? TruckType { get; set; }
+    }
+}
