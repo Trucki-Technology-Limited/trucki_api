448f967095c86adb5d01a210db52fac08c08db36
kunle.edun <kunle.edun@sterling.ng>
2024-03-19 12:54:52 +0100

    features updates on Company, Business and Manager

--- trucki/Profiles/Profile.cs
+++ trucki/Profiles/Profile.cs
@@ -24,6 +24,7 @@ public class Profiles : Profile
         CreateMap<User, ResetPasswordDto>().ReverseMap();
         CreateMap<RoutesResponse, BusinessResponse>().ReverseMap();
         CreateMap<Business, BusinessResponse>().ReverseMap();
+        CreateMap<Business, CreateBusinessDto>().ReverseMap();
 
     }
 }
