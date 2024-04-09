b6f167c85d204f8276ddb56bd94dc33597b289cd
agjohngabriel <agjohngabriel@gmail.com>
2024-04-09 18:54:31 +0100

    added migration

---
+++ trucki/Migrations/20240409175410_AddField.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trucki.Migrations.TruckiDB
+{
+    public partial class AddField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Drivers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Trucks",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Documents = table.Column<List<string>>(type: "text[]", nullable: false),
+                    CertOfOwnerShip = table.Column<string>(type: "text", nullable: false),
+                    PlateNumber = table.Column<string>(type: "text", nullable: false),
+                    TruckCapacity = table.Column<string>(type: "text", nullable: false),
+                    DriverId = table.Column<string>(type: "text", nullable: true),
+                    Capacity = table.Column<string>(type: "text", nullable: false),
+                    TruckOwnerId = table.Column<string>(type: "text", nullable: false),
+                    TruckType = table.Column<int>(type: "integer", nullable: false),
+                    TruckLicenseExpiryDate = table.Column<string>(type: "text", nullable: false),
+                    RoadWorthinessExpiryDate = table.Column<string>(type: "text", nullable: false),
+                    InsuranceExpiryDate = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trucks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Drivers");
+        }
+    }
+}
