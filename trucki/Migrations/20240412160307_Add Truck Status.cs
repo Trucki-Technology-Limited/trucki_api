8a6fb62c279504728e8e39434955d918dcc167a4
agjohngabriel <agjohngabriel@gmail.com>
2024-04-12 17:03:24 +0100

    added migration

---
+++ trucki/Migrations/20240412160307_Add Truck Status.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trucki.Migrations.TruckiDB
+{
+    public partial class AddTruckStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TruckStatus",
+                table: "Trucks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TruckStatus",
+                table: "Trucks");
+        }
+    }
+}
