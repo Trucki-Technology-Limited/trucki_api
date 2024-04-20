85bc7434459f930f9c0b7e470e7d82e905265c90
agjohngabriel <agjohngabriel@gmail.com>
2024-04-20 19:37:47 +0100

    added order migration

---
+++ trucki/Migrations/20240420183657_AddOrdersModel.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trucki.Migrations.TruckiDB
+{
+    public partial class AddOrdersModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DriverId1",
+                table: "Trucks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    OrderId = table.Column<string>(type: "text", nullable: false),
+                    TruckNo = table.Column<string>(type: "text", nullable: false),
+                    Quantity = table.Column<string>(type: "text", nullable: false),
+                    CargoType = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    OrderStatus = table.Column<int>(type: "integer", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    TruckId = table.Column<string>(type: "text", nullable: true),
+                    RouteId = table.Column<string>(type: "text", nullable: true),
+                    RoutesId = table.Column<string>(type: "text", nullable: true),
+                    OfficerId = table.Column<string>(type: "text", nullable: false),
+                    ManagerId = table.Column<string>(type: "text", nullable: false),
+                    CustomerId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Managers_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "Managers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Officers_OfficerId",
+                        column: x => x.OfficerId,
+                        principalTable: "Officers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_RoutesEnumerable_RoutesId",
+                        column: x => x.RoutesId,
+                        principalTable: "RoutesEnumerable",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Orders_Trucks_TruckId",
+                        column: x => x.TruckId,
+                        principalTable: "Trucks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trucks_DriverId1",
+                table: "Trucks",
+                column: "DriverId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerId",
+                table: "Orders",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ManagerId",
+                table: "Orders",
+                column: "ManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OfficerId",
+                table: "Orders",
+                column: "OfficerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_RoutesId",
+                table: "Orders",
+                column: "RoutesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_TruckId",
+                table: "Orders",
+                column: "TruckId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks",
+                column: "DriverId1",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Trucks_DriverId1",
+                table: "Trucks");
+
+            migrationBuilder.DropColumn(
+                name: "DriverId1",
+                table: "Trucks");
+        }
+    }
+}
