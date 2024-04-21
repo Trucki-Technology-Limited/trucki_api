7388e401b782990c5386b85c174abed5365d6f8c
agjohngabriel <agjohngabriel@gmail.com>
2024-04-21 09:40:21 +0100

    updated migration

---
+++ trucki/Migrations/20240421083952_update migration.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trucki.Migrations.TruckiDB
+{
+    public partial class updatemigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DriverId1",
+                table: "Trucks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks",
+                column: "DriverId1",
+                principalTable: "Drivers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DriverId1",
+                table: "Trucks",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Trucks_Drivers_DriverId1",
+                table: "Trucks",
+                column: "DriverId1",
+                principalTable: "Drivers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
