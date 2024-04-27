fae7b4b7ec3b7448a2370231f92aa3602ad6ae8d
agjohngabriel <agjohngabriel@gmail.com>
2024-04-27 09:57:42 +0100

    updated migration

---
+++ trucki/Migrations/20240427085724_update new migration.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trucki.Migrations.TruckiDB
+{
+    public partial class updatenewmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ManagerId",
+                table: "Orders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryAddress",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryAddress",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ManagerId",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Managers_ManagerId",
+                table: "Orders",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
