b87d683e84cc20610665a2f68ec924e18e0a2d50
agjohngabriel <agjohngabriel@gmail.com>
2024-09-19 22:01:13 +0100

    modified AllOfficerResponseModel

--- trucki/Models/ResponseModels/AllOfficerResponseModel.cs
+++ trucki/Models/ResponseModels/AllOfficerResponseModel.cs
@@ -8,6 +8,7 @@ namespace trucki.Models.ResponseModels
         public string OfficerName { get; set; }
         public string PhoneNumber { get; set; }
         public string EmailAddress { get; set; }
+        public string UserId { get; set; } 
         public OfficerType OfficerType { get; set; }
         public string CompanyId { get; set; }
     }
